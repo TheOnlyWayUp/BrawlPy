0c9e54d932e38161af6ed9a6ae01b2f2681beec3
TheOnlyWayUp <76237496+TheOnlyWayUp@users.noreply.github.com>
2022-02-10 18:29:01 +0530

    Update player.py

--- examples/player.py
+++ examples/player.py
@@ -7,17 +7,13 @@ async def brawlPy():
     player = await client.get_player("JP20RUR2")
     
     print(player)
-
     print(player.name,player.tag, player.trophies)
     
     for brawler in player.brawlers:
-    
         print(brawler)
 
     print(player.battleLog) # A big fat list
-    
     print(player.club) # returns None cuz I am not in a club :( , try changing the tag
 
     
-loop = asyncio.get_event_loop()
-loop.run_until_complete(brawlPy())
+asyncio.run(brawlPy())
