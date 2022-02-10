2e7aee5bbfd5badc7b15877ecb7b822e00dbca8e
TheOnlyWayUp <76237496+TheOnlyWayUp@users.noreply.github.com>
2022-02-10 18:30:20 +0530

    Update utils.py

--- brawlpy/utils.py
+++ brawlpy/utils.py
@@ -5,10 +5,10 @@ def checkTag(tag : str):
     allowed_chars = "0289PYLQGRJCUV"
 
     if len(tag) < 3:
-        raise TagNotFoundError(404, reason="Tag can't be less then 3 characters!")
+        raise TagNotFoundError(404, reason="Tag can't be less then 3 characters.")
     invalid = [i for i in tag if i not in allowed_chars]
-    if invalid:
-        raise TagNotFoundError(404, reason="An Invalid character has been passed!",invalid_characters=invalid)
+    if len(invalid) >= 1: # More Readable
+        raise TagNotFoundError(404, reason="An Invalid character has been passed.", invalid_characters=invalid)
 
     if not tag.startswith("%23"):
         tag = "%23" + tag
