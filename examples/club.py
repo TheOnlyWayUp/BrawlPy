e09d7cc568fc14c28cc3283e7548d3f8616e428f
TheOnlyWayUp <76237496+TheOnlyWayUp@users.noreply.github.com>
2022-02-10 18:27:50 +0530

    Update club.py

--- examples/club.py
+++ examples/club.py
@@ -4,17 +4,14 @@ import asyncio
 client = brawlpy.Client('your_token_here') # Enter your token here
 
 async def brawlPy():
-    club = await client.get_club("PVQ0RP90")
-    
-    print(club)
-
-    print(club.name,club.tag, club.trophies)
+    club = await client.get_club("PVQ0RP90") # This gets the club, from the ID
+    print(club) # Prints the club object
+    print(club.description) # Prints the club description
+    print('Attributes -', ', '.join(list(dir(club)))) # This prints all the attributes and methods, anything you can access with club.***
+    print(club.name, club.tag, club.trophies) # Prints the club name, tag and trophies.
     
     for member in club.members:
+        print(member) # Prints the name of every member in the club
     
-        print(member)
     
-    print(club.description)
-    
-loop = asyncio.get_event_loop()
-loop.run_until_complete(brawlPy())
+asyncio.run(brawlPy())
